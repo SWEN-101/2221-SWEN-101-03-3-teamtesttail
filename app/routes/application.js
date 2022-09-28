e913e200f2315352df847aba6e5cebe1c8bfe4d2
Liam Shackhorn <lhw2837@rit.edu>
2022-09-28 16:43:08 -0400

    Visual timer is working!

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,24 +1,27 @@
 import Route from '@ember/routing/route';
 
 export default class VisualTimerRoute extends Route {
-
+    
     model() {
-        return [
-            { number: "1", letter: null },
-            { number: "2", letter: null },
-            { number: "3", letter: null },
-            { number: "4", letter: null },
-            { number: "5", letter: null },
-            { number: "6", letter: null },
-            { number: "7", letter: null },
-            { number: "8", letter: null },
-            { number: "9", letter: null },
-            { number: "10", letter: null },
-            { number: "11", letter: null },
-            { number: "12", letter: null },
-            { number: "13", letter: null },
-            { number: "14", letter: null },
-            { number: "15", letter: null }
-        ];
+        return {
+            sections: [ // Not implemented yet.
+                [1, 2, 3],
+                [4, 5],
+                [6, 7],
+                [8, 9, 10],
+        ],
+            questions: [
+                { number: 1, letter: null, percentage: 10 },
+                { number: 2, letter: null, percentage: 10 },
+                { number: 3, letter: null, percentage: 10 },
+                { number: 4, letter: null, percentage: 10 },
+                { number: 5, letter: null, percentage: 10 },
+                { number: 6, letter: null, percentage: 10 },
+                { number: 7, letter: null, percentage: 10 },
+                { number: 8, letter: null, percentage: 10 },
+                { number: 9, letter: null, percentage: 10 },
+                { number: 10, letter: null, percentage: 10 }
+            ]
+        };
     }
 }
